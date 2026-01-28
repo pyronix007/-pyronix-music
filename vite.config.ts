f7866026f0eca1aba5e1860e2b431573d8396fa6
pyronixmusix <preux.cedric@gmail.com>
2026-01-29 00:51:50 +0100

    Refactor vite.config.ts for consistency and updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+
+  // ✅ Obligatoire pour GitHub Pages
+  base: "/-pyronix-music/",
+
   define: {
-    'process.env': process.env
-  }
-});
+    "process.env": process.env,
+  },
+})
