b3f3fa4aa580abeeffe603e75142b48b1bfba1dd
pyronix007 <preux.cedric@gmail.com>
2026-01-29 01:21:46 +0100

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
 
-  // ✅ Obligatoire pour GitHub Pages
+  // ✅ GitHub Pages base path
   base: "/-pyronix-music/",
 
   define: {
